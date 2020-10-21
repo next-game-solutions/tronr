4dd56560d87af5e9842a3d9303be299473626d1a
Sergey Mastitsky <mastitsky@gmail.com>
2020-10-21 16:32:09 +0100

    added to_unix_timestamp() and tests

---
+++ R/to_unix_timestamp.R
@@ -0,0 +1,43 @@
+#' Convert to Unix timestamp
+#'
+#' Converts a POSIX datetime to Unix timestamp
+#'
+#' @param datetime Either a `POSIXct` datetime value with a second-level
+#'     precision or a character value that can be coerced into such a `POSIXct`
+#'     datetime value. Expected format: `%Y-%m-%d %H:%M:%S`. Hours, minutes,
+#'     and seconds must be provided explicitly (so, for example, a value of
+#'     `2010-01-01 00:00:00` will work, while `2010-01-01` will throw
+#'     an error).
+#' @param tz Character value corresponding to the timezone of `datetime`.
+#'     Defaults to `UTC`.
+#'
+#' @return A character value that corresponds to a Unix timestamp.
+#'
+#' @details The TronGrid API commands expect Unix timestamps with a
+#'     millisecond-level precision. This function will therefore automatically
+#'     add 3 more zeros after converting `datetime` into a Unix timestamp.
+#'
+#' @export
+#'
+#' @examples
+#' dt <- "2019-01-01 00:00:10"
+#' to_unix_timestamp(datetime = dt, tz = "UTC")
+#'
+to_unix_timestamp <- function(datetime, tz = "UTC") {
+
+  if (!(is.character(datetime) | inherits(datetime, "POSIXct"))) {
+    stop("`datetime` is neither a character nor a POSIXct value", call. = FALSE)
+  }
+
+  ts <- lubridate::as_datetime(datetime, format = "%Y-%m-%d %H:%M:%S")
+
+  if (is.na(ts)) {
+    stop("`datetime` cannot be coerced to a POSIXct value", call. = FALSE)
+  }
+
+  ts <- lubridate::round_date(ts, unit = "second")
+  ts <- as.character(unclass(ts) * 1000)
+
+  return(ts)
+
+}
---
+++ tests/testthat/test-to_unix_timestamp.R
@@ -0,0 +1,14 @@
+test_that("to_unix_timestamp() throws errors when expected", {
+  expect_error(to_unix_timestamp("abc"))
+  expect_error(to_unix_timestamp(1))
+  expect_error(to_unix_timestamp("2010-01-01"))
+  expect_error(to_unix_timestamp("2010-01-01 00:00:70"))
+  expect_error(to_unix_timestamp("00:00:10"))
+})
+
+test_that("to_unix_timestamp() returns correct values", {
+  expect_identical(to_unix_timestamp("1970-01-01 00:00:01"),
+                   as.character(1000))
+  expect_identical(to_unix_timestamp("1970-01-02 00:00:00"),
+                   as.character(60*60*24*1000))
+})
