e02f94f3a4627bbe09b8af9cbfacba0d3c01dd1d
Sergey Mastitsky <mastitsky@gmail.com>
2020-11-20 18:59:36 +0000

    first stab at parse_tx_info

--- R/get_tx_info_by_account_address.R
+++ R/get_tx_info_by_account_address.R
@@ -24,9 +24,6 @@
 #'     period to retrieve the transactions from.
 #' @param limit An integer - number of transactions per page. Defaults to 20.
 #'     Maximum accepted value is 200.
-#' @param search_internal A boolean value. If `TRUE` (default), query params are
-#'     applied to both normal and internal transactions. If `FALSE`, query
-#'     params are only applied to normal transactions.
 #' @param max_attempts A non-zero, positive integer specifying the maximum
 #'     number of additional attempts to call the API if the first attempt fails
 #'     (i.e. its call status is different from `200`). Additional attempts are
@@ -39,7 +36,7 @@
 #'     advised to implement their own logic if they need to retrieve data
 #'     covering a larger period of time.
 #'
-#' @return A data frame where each row corresponds to one transaction.
+#' @return A tibble where each row corresponds to one transaction.
 #' @export
 #'
 #' @examples tx_df <- get_tx_info_by_account_address(
@@ -59,42 +56,50 @@ get_tx_info_by_account_address <- function(address,
                                            only_from = FALSE,
                                            min_timestamp = 0,
                                            max_timestamp = NULL,
-                                           limit = 10L,
-                                           search_internal = TRUE,
+                                           limit = 200L,
                                            max_attempts = 3L) {
 
   # Validate inputs:
-
   stopifnot(is.character(address))
   stopifnot(is.logical(only_confirmed) | is.null(only_confirmed))
   stopifnot(is.logical(only_unconfirmed) | is.null(only_unconfirmed))
   stopifnot(is.logical(only_to))
   stopifnot(is.logical(only_from))
   stopifnot(is.integer(limit))
-  stopifnot(is.logical(search_internal))
   stopifnot(is.integer(max_attempts) & max_attempts > 0)
 
-  if (!(is.character(min_timestamp) | is.numeric(min_timestamp))) {
+
+  if (!(is.character(min_timestamp) |
+        is.numeric(min_timestamp) |
+        is.null(min_timestamp)) ) {
     stop("`min_timestamp` is neither a numeric nor a character value",
          call. = FALSE)
   }
 
-  min_dt <- suppressWarnings(as.numeric(min_timestamp) / 1000)
 
-  if (is.na(min_dt)) {
-    stop("`min_timestamp` cannot be coerced to a POSIXct value", call. = FALSE)
+  if (!is.null(min_timestamp)) {
+    min_dt <- suppressWarnings(as.numeric(min_timestamp) / 1000)
+    if (is.na(min_dt)) {
+      stop("`min_timestamp` cannot be coerced to a POSIXct value", call. = FALSE)
+    }
   }
 
-  if (!(is.character(max_timestamp) | is.numeric(max_timestamp))) {
+
+  if (!(is.character(max_timestamp) |
+        is.numeric(max_timestamp) |
+        is.null(max_timestamp))) {
     stop("`max_timestamp` is neither a numeric nor a character value",
          call. = FALSE)
   }
 
-  max_dt <- suppressWarnings(as.numeric(max_timestamp) / 1000)
+  if (!is.null(max_timestamp)) {
+    max_dt <- suppressWarnings(as.numeric(max_timestamp) / 1000)
 
-  if (is.na(max_dt)) {
-    stop("`max_timestamp` cannot be coerced to a POSIXct value", call. = FALSE)
+    if (is.na(max_dt)) {
+      stop("`max_timestamp` cannot be coerced to a POSIXct value", call. = FALSE)
+    }
   }
+
 
   if (is.logical(only_confirmed) & is.logical(only_unconfirmed)) {
     stop("`only_confirmed` and `only_unconfirmed` cannot be used simultaneously",
@@ -109,7 +114,7 @@ get_tx_info_by_account_address <- function(address,
                        only_from = tolower(only_from),
                        min_timestamp = min_timestamp,
                        max_timestamp = max_timestamp,
-                       search_internal = tolower(search_internal),
+                       search_internal = tolower(FALSE),
                        limit = limit)
 
   url <- tronr::build_get_request(base_url = "https://api.trongrid.io",
---
+++ R/parse_tx_info.R
@@ -0,0 +1,75 @@
+#' Parse transaction attributes
+#'
+#' Converts a list with transaction attributes into a tibble
+#'
+#' @param info A list returned as a result of calling the
+#'     "get transaction info by account address" method.
+#'
+#' @details This is a non-public function that is used to simplify syntax of
+#'     the `get_tx_info_by_account_address()` function.
+#'
+#' @return A tibble where each row corresponds to one transaction.
+#' @export
+#'
+#' @examples query_params <- list(only_confirmed = tolower(TRUE),
+#'                                only_to = tolower(TRUE),
+#'                                min_timestamp = "1577836800000",
+#'                                max_timestamp = "1577838600000",
+#'                                limit = 100L)
+#' address <- "TAUN6FwrnwwmaEqYcckffC7wYmbaS6cBiX"
+#' url <- tronr::build_get_request(base_url = "https://api.trongrid.io",
+#'                                 path = c("v1", "accounts",
+#'                                 address, "transactions"),
+#'                                 query_parameters = query_params)
+#' r <- tronr::api_request(url = url, max_attempts = max_attempts)
+#' tx_info <- r$data
+#' tx_info_parsed <- parse_tx_info(tx_info)
+#' print(tx_info_parsed)
+#'
+parse_tx_info <- function(info) {
+
+  stopifnot(is.list(info))
+
+  null_checker <- function(x) {
+    ifelse(!is.null(x), as.character(x), NA_character_)
+  }
+
+  info_names <- names(info)
+
+  raw_data <- info$raw_data$contract[[1]]$parameter$value %>%
+    tibble::as_tibble() %>%
+    dplyr::mutate_if(., is.numeric, .funs = ~ as.character(gmp::as.bigz(.)))
+
+  tx_result <- null_checker(info$ret[[1]]$contractRet)
+
+  net_usage = null_checker(info$net_usage)
+
+  net_fee = null_checker(info$net_fee)
+
+  energy_usage = null_checker(info$energy_usage)
+
+  energy_fee = null_checker(info$energy_fee)
+
+  internal_txs <- ifelse(length(info$internal_transactions) != 0L,
+                        info$internal_transactions,
+                        NA)
+
+  res <- tibble::tibble(
+
+    tx_id = info$txID,
+    tx_type = info$raw_data$contract[[1]]$type,
+    tx_result = tx_result,
+    net_usage = net_usage,
+    net_fee = net_fee,
+    energy_usage = energy_usage,
+    energy_fee = energy_fee,
+    block_number = info$blockNumber %>% gmp::as.bigz() %>% as.character(),
+    block_timestamp = tronr::from_unix_timestamp(info$block_timestamp),
+    raw_data = list(raw_data),
+    internal_txs = internal_txs
+
+  )
+
+  return(res)
+
+}
