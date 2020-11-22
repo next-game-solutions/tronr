3ad739eea79130822735063bd9584d0926fed87f
Sergey Mastitsky <mastitsky@gmail.com>
2020-11-22 12:20:09 +0000

    tests for get_tx_info_by_account_address

--- tests/testthat/test-get_tx_info_by_account_address.R
+++ tests/testthat/test-get_tx_info_by_account_address.R
@@ -1,3 +1,128 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+tx_df <- get_tx_info_by_account_address(
+  address = "TAUN6FwrnwwmaEqYcckffC7wYmbaS6cBiX",
+  only_confirmed = TRUE,
+  only_from = TRUE,
+  min_timestamp = "1577836800000",
+  max_timestamp = "1577838600000",
+  limit = 50L
+)
+
+
+test_that("get_tx_info_by_account_address throws errors as expected", {
+
+  # address:
+  expect_error(
+    get_tx_info_by_account_address(address = as.factor("a"))
+  )
+
+  # only_confirmed:
+  expect_error(
+    get_tx_info_by_account_address(
+      address = "TAUN6FwrnwwmaEqYcckffC7wYmbaS6cBiX",
+      only_confirmed = 1L)
+  )
+
+  # only_unconfirmed:
+  expect_error(
+    get_tx_info_by_account_address(
+      address = "TAUN6FwrnwwmaEqYcckffC7wYmbaS6cBiX",
+      only_unconfirmed = 1L)
+  )
+
+  # only_to:
+  expect_error(
+    get_tx_info_by_account_address(
+      address = "TAUN6FwrnwwmaEqYcckffC7wYmbaS6cBiX",
+      only_to = 1L)
+  )
+
+  # only_from:
+  expect_error(
+    get_tx_info_by_account_address(
+      address = "TAUN6FwrnwwmaEqYcckffC7wYmbaS6cBiX",
+      only_from = 1L)
+  )
+
+  # min_timestamp:
+  expect_error(
+    get_tx_info_by_account_address(
+      address = "TAUN6FwrnwwmaEqYcckffC7wYmbaS6cBiX",
+      min_timestamp = TRUE)
+  )
+
+  expect_error(
+    get_tx_info_by_account_address(
+      address = "TAUN6FwrnwwmaEqYcckffC7wYmbaS6cBiX",
+      min_timestamp = "abcd")
+  )
+
+  # max_timestamp:
+  expect_error(
+    get_tx_info_by_account_address(
+      address = "TAUN6FwrnwwmaEqYcckffC7wYmbaS6cBiX",
+      max_timestamp = TRUE)
+  )
+
+  expect_error(
+    get_tx_info_by_account_address(
+      address = "TAUN6FwrnwwmaEqYcckffC7wYmbaS6cBiX",
+      max_timestamp = "abcd")
+  )
+
+  # limit
+  expect_error(
+    get_tx_info_by_account_address(
+      address = "TAUN6FwrnwwmaEqYcckffC7wYmbaS6cBiX",
+      limit = "50")
+  )
+
+  # max_attempts
+  expect_error(
+    get_tx_info_by_account_address(
+      address = "TAUN6FwrnwwmaEqYcckffC7wYmbaS6cBiX",
+      max_attempts = "50")
+  )
+
+  expect_error(
+    get_tx_info_by_account_address(
+      address = "TAUN6FwrnwwmaEqYcckffC7wYmbaS6cBiX",
+      max_attempts = -1L)
+  )
+
+})
+
+
+test_that("get_tx_info_by_account_address returns correct object", {
+
+  expect_equal(nrow(tx_df), 9L)
+  expect_equal(ncol(tx_df), 11L)
+
+  expect_named(tx_df, expected = c("tx_id",
+                                   "tx_type",
+                                   "tx_result",
+                                   "net_usage",
+                                   "net_fee",
+                                   "energy_usage",
+                                   "energy_fee",
+                                   "block_number",
+                                   "block_timestamp",
+                                   "raw_data",
+                                   "internal_txs"))
+
+  expect_true(all(tx_df$tx_type == "TransferContract"))
+  expect_true(all(tx_df$tx_result == "SUCCESS"))
+  expect_equal(sum(as.numeric(tx_df$net_usage)), 0)
+  expect_equal(sum(as.numeric(tx_df$energy_usage)), 0)
+  expect_equal(min(as.numeric(tx_df$block_number)), 15860591L)
+  expect_equal(max(as.numeric(tx_df$block_number)), 15860895L)
+
+  expect_is(tx_df$raw_data, "list")
+  expect_named(tx_df$raw_data[[1]], c("timestamp",
+                                      "amount",
+                                      "from_address",
+                                      "to_address"))
+
+  expect_is(tx_df$block_timestamp, "POSIXct")
+  expect_is(tx_df$raw_data[[1]]$timestamp, "POSIXct")
+
 })
