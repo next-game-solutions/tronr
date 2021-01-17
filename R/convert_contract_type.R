1ffb440df239f9d68548e5816a8e30fdc6513f31
Sergey Mastitsky <mastitsky@gmail.com>
2021-01-17 17:00:54 +0000

    function convert_contract_address

---
+++ R/convert_contract_type.R
@@ -0,0 +1,53 @@
+convert_contract_type <- function(id) {
+
+  if (!is.numeric(id) && is.na(suppressWarnings(as.numeric(id)))) {
+    rlang::abort("`id` must be a numeric or coercible-to-numeric value")
+  }
+
+  dict <- list(
+    `0` = "AccountCreateContract",
+    `1` = "TransferContract",
+    `2` = "TransferAssetContract",
+    `3` = "VoteAssetContract",
+    `4` = "VoteWitnessContract",
+    `5` = "WitnessCreateContract",
+    `6` = "AssetIssueContract",
+    `8` = "WitnessUpdateContract",
+    `9` = "ParticipateAssetIssueContract",
+    `10` = "AccountUpdateContract",
+    `11` = "FreezeBalanceContract",
+    `12` = "UnfreezeBalanceContract",
+    `13` = "WithdrawBalanceContract",
+    `14` = "UnfreezeAssetContract",
+    `15` = "UpdateAssetContract",
+    `16` = "ProposalCreateContract",
+    `17` = "ProposalApproveContract",
+    `18` = "ProposalDeleteContract",
+    `19` = "SetAccountIdContract",
+    `20` = "CustomContract",
+    `21` = "BuyStorageContract",
+    `22` = "BuyStorageBytesContract",
+    `23` = "SellStorageContract",
+    `30` = "CreateSmartContract",
+    `31` = "TriggerSmartContract",
+    `32` = "GetContract",
+    `33` = "UpdateSettingContract",
+    `41` = "ExchangeCreateContract",
+    `42` = "ExchangeInjectContract",
+    `43` = "ExchangeWithdrawContract",
+    `44` = "ExchangeTransactionContract",
+    `45` = "UpdateEnergyLimitContract",
+    `46` = "AccountPermissionUpdateContract",
+    `47` = "PermissionAddKeyContract",
+    `48` = "PermissionUpdateKeyContract",
+    `49` = "PermissionDeleteKeyContract"
+  )
+
+  id <- as.character(id)
+
+  result <- ifelse(id %in% names(dict),
+                   dict[id], NA_character_)
+
+  return(as.character(result))
+
+}
